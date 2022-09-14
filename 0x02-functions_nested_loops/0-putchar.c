ab257d35d4450a4c79a4afa1cc87b7d185bc2854
preshy101 <presh449@gmail.com>
2022-09-13 19:19:39 -0700

    functions and loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int myNumbers[] = {15, 312, 7, 11, 159, 102, 197, 21};
+
+	int i = 0;
+
+	while (i < 8)
+	{
+		putchar(myNumbers[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
