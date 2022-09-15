98257c80d42964dce06f31eb2cafb5304d051d92
preshy101 <presh449@gmail.com>
2022-09-15 13:11:59 -0700

    more function

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  print_most_numbers-prints number
+    * Return:
+  */
+void print_most_numbers(void)
+{
+	char n = 0;
+
+	while (n <= 9)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar('0' + n);
+		}
+		n++;
+	}
+	_putchar('\n');
+}
