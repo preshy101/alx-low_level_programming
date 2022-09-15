fd3bd3b16f9641992cbb2580d24bb54d02689f39
preshy101 <presh449@gmail.com>
2022-09-15 05:12:58 -0700

    all other updates

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /**
@@ -9,18 +8,18 @@
  */
 void jack_bauer(void)
 {
-		int h, m;
+	int h, m;
 
-	for (h = 0; h < 24; h++)
-	{
-	for (m = 0; m < 60; m++)
-	{
-		_putchar((h / 10) + '0');
-		_putchar((h % 10) + '0');
-		_putchar(':');
-		_putchar((m / 10) + '0');
-		_putchar((m % 10) + '0');
-		_putchar('\n');
-	}
-	}
+		for (h = 0; h < 24; h++)
+		{
+			for (m = 0; m < 60; m++)
+			{
+				_putchar((h / 10) + '0');
+				_putchar((h % 10) + '0');
+				_putchar(':');
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0');
+				_putchar('\n');
+			}
+		}
 }
