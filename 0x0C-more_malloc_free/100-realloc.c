1afebb6cdc1e86355144c4a8ff8f0c99a3aa1a61
preshy101 <presh449@gmail.com>
2023-01-06 05:49:21 -0800

    Write a function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -26,6 +26,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *ptr2;
+
 	if (old_size == new_size)
 		return (ptr);
 	if (ptr == NULL)
